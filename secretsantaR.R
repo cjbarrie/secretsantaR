2d8710930994b9c912daa186eb81c22bee310b2b
Christopher Barrie <chrisjbarrie@googlemail.com>
2020-11-28 16:21:44 +0200

    Change to correct public script

--- secretsantaR.R
+++ secretsantaR.R
@@ -1,11 +1,9 @@
-library(gmailr)
-
 #define function to get random pairs where everyone gifts one and everyone receives one
 #without duplication
 
-secretsantaR <- function(names){
-  emails_names <- names
-  sampled_names <- sample(names[,2])
+secretsantaR <- function(x){
+  emails_names <- x
+  sampled_names <- sample(x[,2])
   santadf <- data.frame(names = sampled_names,
                         receiver.names = c(sampled_names[-1], 
                                            sampled_names[1]))
@@ -17,30 +15,38 @@ secretsantaR <- function(names){
 }
 #get list of Santa and recipient pairs with emails then names
 
-names <- data.frame(emails =c("chrisjbarrie@googlemail.com", "georgiabarrie@gmail.com",
-                  "grahammcmbarrie@hotmail.co.uk", "mandybarrie@hotmail.com"),
-                  names = c("Chris", "Georgia", "Graham", "Mandy"))
+names <- data.frame(emails =c("email1", "email2",
+                              "email3", "email4"),
+                    names = c("Name1", "Name2", "Name3", "Name4"))
 
 
+#check working:
+secretsantaR(names)
 
-gm_auth_configure(path = "/Users/christopherbarrie/Dropbox/sandbox/secret_santa/credentials.json")
-gm_auth()
 
+library(gmailr)
+
+#set up API token for gmail account, following instructions here:
+#https://github.com/r-lib/gmailr
+gm_auth_configure(path = "PATH_TO_CREDIENTALS/credentials.json")
+gm_auth() #authorize token
+
+#get names randomly sorted into pairs
 sorted_names <- secretsantaR(names)
 
 for (i in 1:nrow(sorted_names)) {
-  
   recipient.email <- sorted_names[i, "emails"]
   recipient <- sorted_names[i, "receiver.names"]
-  gifter <- sorted_names[i, "giver.names"]
+  giver <- sorted_names[i, "giver.names"]
   
   email_to_send <-
     gm_mime() %>%
     gm_to(recipient.email) %>%
-    gm_from("chrisjbarrie@gmail.com") %>%
-    gm_subject("Secret Book Santa email") %>%
-    gm_text_body(paste("Santa is here, and he's here to tell you that you're buying a book for",
+    gm_from("SENDER_ACCOUNT@gmail.com") %>% #use one gmail account to send all messages
+    gm_subject("Secret Book email") %>%
+    gm_text_body(paste0("Hello and MERRY CHRISTMAS, ", giver, ". Santa is here, and he's here to tell you that you're buying a present for: ",
                        recipient, "! \n 
-               HO HO HO MERRY READING!"))
+               HO HO HO MERRY CHRISTMAS!"))
   gm_send_message(email_to_send)
 }
+
